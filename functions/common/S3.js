fbede4ec779088968f373685345c1ef5943ab9ee
rajan shah <abhishek.desai@appgambit.com>
2022-11-28 18:40:55 +0530

    S3 get file feature added

--- functions/common/S3.js
+++ functions/common/S3.js
@@ -3,7 +3,22 @@ const AWS = require('aws-sdk')
 const s3Client = new AWS.S3
 
 const S3 = {
-    async get (){
+    async get (fileName, bucket){
+        const params = {
+            Bucket: bucket,
+            Key: fileName
+        }
+
+        let data = await s3Client.getObject(params).promise()
+
+        if(!data){
+            throw Error(`Failed to get ${fileName} from ${bucket}`)
+        }
+
+        if(fileName.slice(fileName.length - 4, fileName.length) == 'json'){
+            data = data.Body.toString()
+        }
+        return data
 
     },
 
---
+++ functions/endpoints/getFile.js
@@ -0,0 +1,26 @@
+const Responses = require('../common/API_Responses')
+const S3 = require('../common/S3')
+
+const bucket = process.env.bucket
+
+const handler = async (event) => {
+    console.log(event);
+    if (!event.pathParameters || !event.pathParameters.fileName) {
+        return Responses._400({ message: 'Missing the fileName from the path' })
+    }
+
+    let fileName = event.pathParameters.fileName
+
+    const file = await S3.get(fileName, bucket).catch((error) => {
+        console.log("Unable to locate file in S3", error)
+        return null
+    })
+
+    if (!file) {
+        return Responses._400({ message: `Failed to find the file ${fileName}` })
+    }
+
+    return Responses._200({ file })
+}
+
+module.exports = { handler }
